eddcad92bc08781b4d4b3b54378a412d332fcbd2
Disklo <rafaellcarvalho5@gmail.com>
2025-09-07 13:30:48 -0300

    Adicionada cor dinâmica aos espaços

--- scripts/espaco.gd
+++ scripts/espaco.gd
@@ -8,10 +8,14 @@ class_name Espaco
 
 @export var nome: String = "Espaço"
 
+@export var cor: Color = "CDE6D0"
+
 @onready var nome_label: RichTextLabel = $NomeLabel
+@onready var cor_espaco: ColorRect = $CorEspaco
 
 func _ready() -> void:
 	nome_label.text = nome
+	cor_espaco.color = cor
 
 # Função virtual que será chamada quando um jogador parar neste espaço.
 # Cada tipo de espaço (propriedade, sorte, etc.) terá sua própria implementação.
