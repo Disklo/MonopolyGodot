4b260054b240122a9bc8cdee83b7a7caa66aa1d9
Disklo <rafaellcarvalho5@gmail.com>
2025-09-29 16:28:05 -0300

    Ajustes na cena de propriedade

--- scripts/propriedade.gd
+++ scripts/propriedade.gd
@@ -6,18 +6,31 @@ class_name Propriedade
 # Variáveis da propriedade, configuráveis no editor
 @export var preco: int = 100
 @export var aluguel_base: int = 10
+@export var comprado: bool = false
+@export var tipo_imovel: String = ""
 
 # O jogador que é o dono dessa propriedade. Fica nulo se não tiver dono.
 var dono: Jogador = null
 
 @onready var preco_label: RichTextLabel = $PrecoLabel
 @onready var aluguel_label: RichTextLabel = $AluguelLabel
+@onready var uma_casa: Sprite2D = $"1casa"
+@onready var duas_casas: Sprite2D = $"2casas"
+@onready var tres_casas: Sprite2D = $"3casas"
+@onready var quatro_casas: Sprite2D = $"4casas"
+@onready var hotel: Sprite2D = $"1hotel"
+@onready var cor_comprada: ColorRect = $CorEspaco
+@onready var cor_a_venda: ColorRect = $CorVenda
+@onready var lotevenda: RichTextLabel = $NomeLabel2
 
 func _ready() -> void:
 	super._ready()
 	preco_label.text = "Preço: R$" + _format_preco(preco)
 	aluguel_label.text = "Aluguel: R$" + str(aluguel_base)
-	mostrar_aluguel()
+	#mostrar_aluguel()
+	if comprado == true:
+		lote_comprado()
+	mostrar_propriedade(tipo_imovel)
 
 # Formata um número para ter o ponto como separador de milhar.
 func _format_preco(numero: int) -> String:
@@ -70,6 +83,32 @@ func cobrar_aluguel(jogador: Jogador) -> void:
 	dono.receber(aluguel_base)
 
 # Retira a visibilidade do preço e coloca a visibilidade no aluguel.
+func lote_comprado() -> void:
+	lotevenda.visible = false
+	preco_label.visible = false
+	aluguel_label.visible = true
+	cor_a_venda.visible = false
+	cor_comprada.visible = true
+
 func mostrar_aluguel() -> void:
 	preco_label.visible = false
 	aluguel_label.visible = true
+
+func mostrar_propriedade(propriedade_a_mostrar: String) -> void:
+	uma_casa.visible = false
+	duas_casas.visible = false
+	tres_casas.visible = false
+	quatro_casas.visible = false
+	hotel.visible = false
+
+	match propriedade_a_mostrar:
+		"uma_casa":
+			uma_casa.visible = true
+		"duas_casas":
+			duas_casas.visible = true
+		"tres_casas":
+			tres_casas.visible = true
+		"quatro_casas":
+			quatro_casas.visible = true
+		"hotel":
+			hotel.visible = true
