61ff7a136b4b09f1a423b492672d8becbef5d986
Disklo <rafaellcarvalho5@gmail.com>
2025-11-24 23:23:15 -0300

    Hotfix: Limite da câmera

--- scripts/camera_controller.gd
+++ scripts/camera_controller.gd
@@ -1,9 +1,18 @@
 extends Camera2D
 
 var zoom_speed = 0.04
-var min_zoom = 0.1
+var min_zoom = 0.05
 var max_zoom = 2.0
 var move_speed = 1000
+
+# Limites da câmera (ajustados para o tamanho do tabuleiro)
+@export var camera_limit_left = -2500
+@export var camera_limit_top = -2500
+@export var camera_limit_right = 2000
+@export var camera_limit_bottom = 2000
+
+func _ready():
+	zoom = Vector2(0.1, 0.1)
 
 func _process(delta):
 	# Mover a câmera com as teclas de seta
@@ -18,6 +27,10 @@ func _process(delta):
 		velocity.y -= 1
 	
 	position += velocity.normalized() * move_speed * delta
+	
+	# Limitar a posição da câmera
+	position.x = clamp(position.x, camera_limit_left, camera_limit_right)
+	position.y = clamp(position.y, camera_limit_top, camera_limit_bottom)
 
 func _input(event):
 	# Controlar o zoom com a roda do mouse
